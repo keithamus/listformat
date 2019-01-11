256d719332d63ebc08610e51d9038f0cbed1246a
Keith Cirkel <keithamus@users.noreply.github.com>
2019-01-11 12:35:58 +0000

    fix: recompile umd bundle

--- umd.js
+++ umd.js
@@ -5,7 +5,7 @@
 }(this, function () { 'use strict';
 
 	const ListFormat = typeof Intl === 'object' && typeof Intl.ListFormat === 'function' && Intl.ListFormat('en');
-	const listFormatItem = (o, x, i, xs) => !i ? x : i && xs.length === 2 ? o + ' and ' + x : i === xs.length -1 ? o + ', and ' + x : o + ', ' + x;
+	const listFormatItem = (o, x, i, xs) => !i ? o + x : i && xs.length === 2 ? o + ' and ' + x : i === xs.length -1 ? o + ', and ' + x : o + ', ' + x;
 	const listFormat = xs => ListFormat ? ListFormat.format(xs) : xs.reduce(listFormatItem, '');
 
 	return listFormat;
